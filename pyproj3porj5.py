6cb786c4269a35a6617d9d0ed23766b3d6413cc6
aidenszolosi <aidenszolosi@gmail.com>
2022-11-13 22:37:25 -0500

    stableish

--- pyproj3porj5.py
+++ pyproj3porj5.py
@@ -3,30 +3,31 @@
 
 #we must import a library, "random"
 
+#Receive input from the user
+#Output data to the console
+##XApply conditional logic
+##XConvert string value to int
+##X - Use the Random library
+#Use while loops
 import random 
 
-NeedToGuess: bool = True
-
 while True:
-    while True:
-        maxValue = input('\nWhat should the highest number for this game be? ')
-        if maxValue.isdigit():
-            maxValue = int(maxValue)
-            RandomNumber = (random.randint(1, maxValue))
+    maxValue = input('\nWhat should the highest number for this game be? ')
+    if maxValue.isdigit():
+        maxValue = int(maxValue)
+        RandomNumber = int((random.randint(1, maxValue)))
+        while True:
             guess = input("Make a guess! What's the number? ")
             guess = int(guess)
-            if guess <= RandomNumber:
-                print('Higher!')
-            
-            elif:
-                guess == RandomNumber
-            
-            else:
-                print('Lower!')
-                break
-        
-        else:
-            print('WTF IDIOT THATS NOT A NUMBER')
+            if guess == RandomNumber:
+                print("You got it!, It was " + str(RandomNumber))  
+                break   
+            elif guess <= RandomNumber:
+                print('\tHigher!')
+            elif guess >= RandomNumber:
+                print('\tLower!')
+    else: 
+        print('That does not seem to be a valid response?')
 
 
 ###print('The number was ' + str(RandomNumber)) 
